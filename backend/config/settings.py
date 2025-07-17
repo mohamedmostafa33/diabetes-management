01f374144b512028fec20babcef3430d7435c74f
mohamedmostafa33 <mohamedabohamama15@gmail.com>
2025-07-17 13:16:48 +0300

    Update ALLOWED_HOSTS for production deployment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['diabetesmanagement.pythonanywhere.com']
 
 
 

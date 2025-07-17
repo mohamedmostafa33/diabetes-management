249861601c274ec268e75750216682595de3f5a5
mohamedmostafa33 <mohamedabohamama15@gmail.com>
2025-07-17 13:19:23 +0300

    Set CORS_ALLOWED_ORIGINS for production security

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -181,4 +181,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True  
+CORS_ALLOWED_ORIGINS = [
+    "https://diabetesmanagement.pythonanywhere.com"
+]
